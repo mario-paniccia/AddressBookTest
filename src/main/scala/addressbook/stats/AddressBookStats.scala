a44cc33e2a74c76ba5119e2c9abd2b63568c212e
Mario Paniccia <mario.paniccia@digital.hmrc.gov.uk>
2017-03-22 23:16:55 +0000

    completed implementation of number of days older operation

--- src/main/scala/addressbook/stats/AddressBookStats.scala
+++ src/main/scala/addressbook/stats/AddressBookStats.scala
@@ -2,6 +2,7 @@ package addressbook.stats
 
 import addressbook.domain.AddressBook
 import addressbook.utils.AddressBookImplicits._
+import org.joda.time.{Days, LocalDate}
 
 object AddressBookStats {
 
@@ -20,12 +21,22 @@ object AddressBookStats {
     }
   }
 
+
   def numberOfDaysOlder(addressBook: AddressBook, p1: String, p2: String): Option[Int] = {
+
+    def calculateDaysOlder(p1Dob: LocalDate, p2Dob: LocalDate): Int = Days.daysBetween(p1Dob, p2Dob).getDays
+
     addressBook.entries match {
       case Nil => None
       case _ => {
-        val p1Entry = addressBook.find(p1)
-        Some(33)
+        val p1EntryOpt = addressBook.find(p1)
+        val p2EntryOpt = addressBook.find(p2)
+        for {
+          p1 <- p1EntryOpt
+          p2 <- p2EntryOpt
+          p1Dob = p1.dob
+          p2Dob = p2.dob
+        } yield calculateDaysOlder(p1Dob, p2Dob)
       }
     }
   }
--- src/test/scala/addressbook/stats/AddressBookStatsSpec.scala
+++ src/test/scala/addressbook/stats/AddressBookStatsSpec.scala
@@ -1,6 +1,6 @@
 package addressbook.stats
 
-import addressbook.domain.{AddressBook, AddressBookEntry, AddressBookSpec}
+import addressbook.domain.{AddressBook, AddressBookEntry}
 import org.joda.time.LocalDate
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -8,7 +8,7 @@ class AddressBookStatsSpec extends FlatSpec with Matchers {
 
   val Dob = new LocalDate(1981, 5, 13)
   val Dob2 = new LocalDate(2000, 5, 13)
-  val Dob3 = new LocalDate(2010, 5, 13)
+  val Dob3 = new LocalDate(2000, 5, 16)
 
   val EmptyAddressBook = AddressBook()
 
@@ -40,4 +40,31 @@ class AddressBookStatsSpec extends FlatSpec with Matchers {
     AddressBookStats.oldestPerson(EmptyAddressBook) shouldBe None
   }
 
+  "AddressBookStats" should "return how many days between two persons" in {
+
+    val ab = AddressBook(AddressBookEntry("p1", "Male", Dob),
+      AddressBookEntry("p2", "Male", Dob2),
+      AddressBookEntry("p3", "Female", Dob3))
+
+    AddressBookStats.numberOfDaysOlder(ab, "p2", "p3") shouldBe Some(3)
+  }
+
+  "AddressBookStats" should "return None for numberOfDaysOlder when address book is empty" in {
+
+    AddressBookStats.numberOfDaysOlder(EmptyAddressBook, "p1", "p2") shouldBe None
+  }
+
+  "AddressBookStats" should "return None for numberOfDaysOlder when address book doesn't contain the 2 persons" in {
+
+    val ab = AddressBook(AddressBookEntry("p1", "Male", Dob))
+
+    AddressBookStats.numberOfDaysOlder(ab, "p4", "p5") shouldBe None
+  }
+
+  "AddressBookStats" should "return None for numberOfDaysOlder when address book doesn't one of the 2 persons" in {
+
+    val ab = AddressBook(AddressBookEntry("p1", "Male", Dob))
+
+    AddressBookStats.numberOfDaysOlder(ab, "p4", "p1") shouldBe None
+  }
 }
